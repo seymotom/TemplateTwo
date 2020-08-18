758856c8372786ee182d00bd2b8749b65d22c461
Tom Seymour <tomseymour44@gmail.com>
2020-08-18 08:22:53 -0400

    Initial Commit

---
+++ TemplateTwo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TemplateTwo
+//
+//  Created by Tom Seymour on 8/18/20.
+//  Copyright © 2020 Tom Seymour. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
